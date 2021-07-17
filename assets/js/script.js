07f5c43397f26fc2a521b7c842438cd0678df84b
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-17 18:28:20 -0400

    added google open sans font and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -279,4 +279,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
