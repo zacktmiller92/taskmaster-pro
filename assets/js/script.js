bed3cbc753549e10a8ef99eb6ab2edbf166bacc4
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-17 17:32:05 -0400

    added moment js and auditTask function to make tasks easier to identify when they are almost due or past due

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,22 @@ var tasks = {};
 
 
 var auditTask = function(taskEl) {
-  // to ensure element is getting to the function
-  console.log(taskEl);
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 
@@ -152,6 +166,8 @@ $(".list-group").on("change", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // JQueryUI 
