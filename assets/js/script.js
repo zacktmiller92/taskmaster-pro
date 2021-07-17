3c01077ef58a7ef5546ca5815087ef7ba33c6748
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-17 18:56:27 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -177,12 +177,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   update: function(event) {
     var tempArr = [];
@@ -246,7 +254,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
